7001fcae6a7e737c1d7b095137c37e8fc7f220f4
Gajus Kuizinas <gajus@gajus.com>
2019-05-07 13:16:43 +0100

    style: use power-assert

--- test/preoom/utilities/parseBytes.js
+++ test/preoom/utilities/parseBytes.js
@@ -4,13 +4,13 @@ import test from 'ava';
 import parseBytes from '../../../src/utilities/parseBytes';
 
 test('parses abbreviated notations Ki, Mi, Gi', (t) => {
-  t.true(parseBytes('1Ki') === 1024);
-  t.true(parseBytes('1Mi') === 1048576);
-  t.true(parseBytes('1Gi') === 1073741824);
+  t.assert(parseBytes('1Ki') === 1024);
+  t.assert(parseBytes('1Mi') === 1048576);
+  t.assert(parseBytes('1Gi') === 1073741824);
 });
 
 test('parses regular notations KiB, MiB, GiB', (t) => {
-  t.true(parseBytes('1Ki') === 1024);
-  t.true(parseBytes('1Mi') === 1048576);
-  t.true(parseBytes('1Gi') === 1073741824);
+  t.assert(parseBytes('1Ki') === 1024);
+  t.assert(parseBytes('1Mi') === 1048576);
+  t.assert(parseBytes('1Gi') === 1073741824);
 });
